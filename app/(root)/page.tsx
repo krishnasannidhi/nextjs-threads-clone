f5c7149c4ae7d240b89a669b0a767ce289bbe80e
krishna <krishna.sannidhi@gmail.com>
2023-09-03 13:54:20 -0500

    logos, middleware, signin and signup clerk js auth

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,5 +1,10 @@
+import { UserButton } from "@clerk/nextjs";
+
 export default function Home() {
-  return <main className="">
-    <h1>Threads</h1>
-  </main>;
+  return (
+    <>
+      {/* <UserButton afterSignOutUrl="/"/> */}
+      <h1 className="head-text text-left">Home</h1>
+    </>
+  );
 }
